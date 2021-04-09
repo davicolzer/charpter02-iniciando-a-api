a7aec7c56df9eaf83f5a3527f9eb23211f107784
Davi Colzer Lira <davicolzer@gmail.com>
2021-04-08 22:30:27 -0300

    Estudando mais

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,28 @@
+// LIBRARY
 import { Router } from "express";
 
+// INTERNAL IMPORTS
+import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+
 const categoriesRoutes = Router();
+const categoriesRepository = new CategoriesRepository();
 
-const categories = [];
-
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    categories.push({
-        name,
-        description,
-    });
+    const createCategoryService = new CreateCategoryService(
+        categoriesRepository
+    );
+
+    createCategoryService.execute({ name, description });
 
     return response.status(201).send();
+});
+
+categoriesRoutes.get("/", (request, response) => {
+    const all = categoriesRepository.list();
+    return response.json(all);
 });
 
 export { categoriesRoutes };
