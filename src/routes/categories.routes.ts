44c4c567f8c8e36bf29d4367deb0037cb7667155
Davi Colzer Lira <davicolzer@gmail.com>
2021-04-18 18:18:29 -0300

    Importando arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,11 +1,17 @@
 // LIBRARY
 import { Router } from "express";
+import multer from "multer";
 
 // INTERNAL IMPORTS
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -13,6 +19,10 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.hundle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
