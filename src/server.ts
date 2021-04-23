1e64137b0fe3af15dc6eed2f040310c780ccf775
Davi Colzer Lira <davicolzer@gmail.com>
2021-04-23 18:57:37 -0300

    aula completa

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running, http://localhost:3333"));
+app.listen(3333, () =>
+    console.log(`
+    Server is running
+    API: http://localhost:3333
+    Documentation: http://localhost:3333/api-docs
+`)
+);
