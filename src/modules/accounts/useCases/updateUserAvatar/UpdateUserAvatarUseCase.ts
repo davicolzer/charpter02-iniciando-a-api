b226a8567fcdc79279d8937c41897ac583bbb424
Davi Colzer Lira <davicolzer@gmail.com>
2021-07-04 16:41:34 -0300

    importando avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 // Adicionar coluna avatar na tabela de users
@@ -22,6 +23,10 @@ class UpdateUserAvatarUseCase {
 
     async execute({ user_id, avatar_file }: IRequest): Promise<void> {
         const user = await this.usersRepository.findById(user_id);
+
+        if (user.avatar) {
+            await deleteFile(`./tmp/avatar/${user.avatar}`);
+        }
 
         user.avatar = avatar_file;
 
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -6,6 +6,7 @@ import { ICategoriesRepository } from "../../repositories/ICategoriesRepository"
 
 interface IImporteCategory {
     name: string;
+
     description: string;
 }
 
@@ -21,32 +22,41 @@ class ImportCategoryUseCase {
             const stream = fs.createReadStream(file.path);
 
             const categories: IImporteCategory[] = [];
+
             const parseFile = csvParse();
 
             stream.pipe(parseFile);
 
             parseFile
+
                 .on("data", async (line) => {
                     const [name, description] = line;
+
                     categories.push({
                         name,
+
                         description,
                     });
                 })
+
                 .on("end", () => {
                     fs.promises.unlink(file.path);
+
                     resolve(categories);
                 })
+
                 .on("error", (err) => {
                     reject(err);
                 });
         });
     }
+
     async execute(file: Express.Multer.File): Promise<void> {
         const categories = await this.loadCategories(file);
 
         categories.map(async (category) => {
             const { name, description } = category;
+
             const existCategory = await this.categoriesRepository.findByName(
                 name
             );
@@ -54,6 +64,7 @@ class ImportCategoryUseCase {
             if (!existCategory) {
                 await this.categoriesRepository.create({
                     name,
+
                     description,
                 });
             }
