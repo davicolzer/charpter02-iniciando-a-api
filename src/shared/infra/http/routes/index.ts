52c0ce95c76be4321c359bd14a65ee5e2dc513b4
Davi Colzer lira <davicolzer@gmail.com>
2021-08-27 13:09:54 -0300

    criando carros

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./authenticate.routes";
+import { carsRoutes } from "./cars.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { specificationsRoutes } from "./specifications.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
+
+router.use("/cars", carsRoutes);
 
 router.use("/categories", categoriesRoutes);
 
